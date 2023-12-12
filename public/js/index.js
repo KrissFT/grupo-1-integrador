58be5bb80d380125fc85a8a106b7dfb184621837
Lukas Santillán <lukassantillan2001@gmail.com>
2023-12-12 14:29:11 -0300

    editado index.html y js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
-//Código del repo 11-11 de Aylen
 async function cargarProductos(){
     let data = await fetch('/api/productos');
     console.log(data)
@@ -10,347 +9,197 @@ async function cargarProductos(){
     let categories = await data.json();
     console.log(categories)
 
-    let ul = document.querySelector("ul#productos");
-    ul.innerHTML = "";
+    let muestra = document.querySelector("#muestra")
+    console.log(products.reverse())
 
-    //Crear producto
-    let createButton = document.createElement("i");
-    createButton.setAttribute("class", "fa-solid fa-plus");
-    createButton.addEventListener('click', () => {
-        let imgDetector = false
-        let div = document.createElement("div");
-        div.className = 'formulario';
-
-        let labelNombre = document.createElement("label")
-        labelNombre.innerHTML = "Nombre: "
-        div.appendChild(labelNombre)
-
-        let inputNombre = document.createElement("input"); // Name, type y id
-        //inputNombre.setAttribute('required', '');
-        div.appendChild(inputNombre);
-
-        let br = document.createElement("br")
-        div.appendChild(br)
-
-        let labelDescription = document.createElement("label")
-        labelDescription.innerHTML = "Descripción: "
-        div.appendChild(labelDescription)
-
-        let inputDescription = document.createElement("input"); // Name, type y id
-        //inputDescription.setAttribute('required', '');
-        div.appendChild(inputDescription);
-
-        let labelPrice = document.createElement("label")
-        labelPrice.innerHTML = "Precio: "
-        div.appendChild(labelPrice)
-
-        let inputPrice = document.createElement("input"); // Name, type y id
-        inputPrice.setAttribute('type', 'number')
-        //inputPrice.setAttribute('required', '');
-        div.appendChild(inputPrice);
-
-        //Categoría
-        let labelCategory = document.createElement("label")
-        labelCategory.setAttribute("for", "categories")
-        let selectCategory = document.createElement("select")
-        selectCategory.setAttribute("id", "categories")
-        selectCategory.setAttribute("name", "categories")
-        //selectCategory.setAttribute('required', '');
-
-        div.appendChild(labelCategory)
-        categories.forEach((category)=>{
-            let categoryOption = document.createElement("option");
-            categoryOption.value = category.id;
-            categoryOption.innerHTML = category.name;
-            selectCategory.appendChild(categoryOption);
-        })
-        div.appendChild(selectCategory);
-
-
-        let inputImg = document.createElement("input");
-        inputImg.setAttribute("name", "image");
-        inputImg.setAttribute("type", "file");
-        inputImg.style.display = "none";
-        inputImg.setAttribute("id", "image-input");
-        inputImg.setAttribute("accept", ".jpg, .png, .jpeg")
-        //inputImg.setAttribute('required', '');
-
-        let labelImg = document.createElement("label");
-        labelImg.setAttribute("for", "image-input")
-        let spanVacio = document.createElement("span");
-        spanVacio.innerHTML = "Subir imagen";
-
-
-        labelImg.appendChild(spanVacio)
-        div.appendChild(labelImg);
-        div.appendChild(inputImg);
-
-        console.log(imgDetector)
-        inputImg.addEventListener("change", () => {
-            imgDetector = true
-            console.log(imgDetector)
-        })
-
-        let boton = document.createElement("button");
-        boton.innerHTML = "Crear"
-        boton.addEventListener("click", async() => {
-            if(inputNombre.value != "" && inputDescription.value != "" && inputPrice.value != "" && imgDetector != false){
-                await fetch('/api/productos', {
-                    method: "POST",
-                    headers:{
-                        "Content-Type": "application/json" // formencode
-                    },
-                    body: JSON.stringify({
-                        name: inputNombre.value,
-                        description: inputDescription.value,
-                        price: inputPrice.value,
-                        category_id: selectCategory.value,
-                        image: "pan.png"
-                    })
-                })
-    
-                data = await fetch('/api/productos');
-                console.log(data)
-                let products = await data.json();
-                console.log(products)
-    
-                let product
-                products.forEach((producto)=>{
-                    if(producto.name == inputNombre.value && producto.description == inputDescription.value && producto.image == "pan.png") {
-                        let resultado = producto
-                        console.log(resultado)
-                        product = resultado
-                    }
-                })
-    
-                console.log(product)
-                let formData = new FormData();
-                formData.append("image",inputImg.files[0])
-                console.log(formData)
-                await fetch('/api/productos/'+product.id, {
-                    method: "PATCH",
-                    //headers:{
-                    //    "Content-Type": "application/json" // formencode
-                    //},
-                    body: formData
-                })
-                cargarProductos();
-            }
-        })
-        div.appendChild(boton);
-        ul.innerHTML = "";
-        ul.appendChild(div);
-    })
-    ul.appendChild(createButton);
-
-
-
-    
-    products.forEach(function(product){
-        let li = document.createElement('li');
-
-        let inputImg = document.createElement("input");
-        inputImg.setAttribute("name", "image");
-        inputImg.setAttribute("type", "file");
-        inputImg.style.display = "none";
-        inputImg.setAttribute("id", "image-input-"+product.id);
-        inputImg.addEventListener("change", async() => {
-            let formData = new FormData();
-            formData.append("image",inputImg.files[0])
-
-            await fetch('/api/productos/'+product.id, {
-                method: "PATCH",
-                //headers:{
-                //    "Content-Type": "application/json" // formencode
-                //},
-                body: formData
-            })
-            cargarProductos();
-        })
-        li.appendChild(inputImg);
-
-        let labelImg = document.createElement("label");
-        labelImg.setAttribute("for", "image-input-"+product.id)
-        if (product.image && product.image != "default.png") {
-            let img = document.createElement("img")
-            img.src = "/img/products/"+product.image;
-            labelImg.appendChild(img);
-        } else {
-            let spanVacio = document.createElement("span");
-            spanVacio.innerHTML = "Subir imagen";
-
-            labelImg.appendChild(spanVacio)
-        }
-        li.appendChild(labelImg);
-
-        // ID
-        let spanId = document.createElement('span');
-        spanId.innerHTML = `${product.id} - `;
-        li.appendChild(spanId);
+    for (let i = 0; i < 3; i++) {
+        let col = document.createElement('div')
+        col.setAttribute('class', "col-md-3 py-3 py-md-0")
         
-        // Nombre
-        let spanName = document.createElement('span');
-        spanName.innerHTML = product.name;
-        li.appendChild(spanName);
+        let card = document.createElement('div')
+        card.setAttribute('class', 'card')
+        
+        let img = document.createElement("img");
+        img.src = "/img/products/"+products[i].image;
 
-        // Precio
-        let spanPrice = document.createElement('span');
-        spanPrice.innerHTML = ` $${product.price}`;
-        li.appendChild(spanPrice);
+        let cardBody = document.createElement('div')
+        cardBody.className = 'card-body'
+        let hNombre = document.createElement('h3')
+        hNombre.innerHTML = products[i].name
+        let pDescripcion = document.createElement('p')
+        pDescripcion.innerHTML = products[i].description
+        let hPrecio = document.createElement('h6')
+        hPrecio.innerHTML = `$${products[i].price}`
+        cardBody.appendChild(hNombre)
+        cardBody.appendChild(pDescripcion)
+        cardBody.appendChild(hPrecio)
 
-        // Categoría
-        let spanCategory = document.createElement('span');
-        spanCategory.innerHTML = ` ${product.Category.name}`;
-        li.appendChild(spanCategory);
+        card.appendChild(img)
+        card.appendChild(cardBody)
+        col.appendChild(card)
 
-        // Icono editar
+        muestra.appendChild(col)
+    }
+
+    let cats = document.querySelector("#categorias")
+    cats.innerHTML = ""
+    cats.className = "row"
+
+    categories.forEach(function(category){
+        let col = document.createElement('div')
+        col.setAttribute('class', "col-md-3 py-3 py-md-0")
+        
+        let card = document.createElement('div')
+        card.setAttribute('class', 'card')
+        
+        let overlay = document.createElement('div')
+        overlay.setAttribute('class', 'overlay')
+        let botonEditar = document.createElement('button')
+        botonEditar.setAttribute('class','btn btn-secondary')
+        botonEditar.setAttribute('title', 'Cambiar nombre')
+        botonEditar.setAttribute('type', 'button')
         let iEditar = document.createElement('i');
         iEditar.setAttribute("class", "fa-solid fa-pen");
-        iEditar.addEventListener("click", async() => {
-            let div = document.createElement("div");
-            div.className = 'formulario';
+        botonEditar.appendChild(iEditar)
+        
+        botonEditar.addEventListener("click", async() => {
+            let div = document.createElement("form");
+            div.setAttribute('id', 'form')
 
+            let divNombre = document.createElement("div");
+            divNombre.className = 'claseFormulario';
             let labelNombre = document.createElement("label")
             labelNombre.innerHTML = "Nombre: "
-            div.appendChild(labelNombre)
-    
+            divNombre.appendChild(labelNombre)
             let inputNombre = document.createElement("input"); // Name, type y id
-            inputNombre.value = product.name;
+            inputNombre.value = category.name;
             inputNombre.setAttribute('required', '');
-            div.appendChild(inputNombre);
-    
-            let labelDescription = document.createElement("label")
-            labelDescription.innerHTML = "Descripción: "
-            div.appendChild(labelDescription)
-    
-            let inputDescription = document.createElement("input"); // Name, type y id
-            inputDescription.value = product.description;
-            inputDescription.setAttribute('required', '');
-            div.appendChild(inputDescription);
-    
-            let labelPrice = document.createElement("label")
-            labelPrice.innerHTML = "Precio: "
-            div.appendChild(labelPrice)
-    
-            let inputPrice = document.createElement("input"); // Name, type y id
-            inputPrice.value = product.price;
-            inputPrice.setAttribute('required', '');
-            div.appendChild(inputPrice);
-    
-            //Categoría
-            let labelCategory = document.createElement("label")
-            labelCategory.setAttribute("for", "categories")
-            let selectCategory = document.createElement("select")
-            selectCategory.setAttribute("id", "categories")
-            selectCategory.setAttribute("name", "categories")
-            selectCategory.setAttribute('required', '');
-    
-            div.appendChild(labelCategory)
-            let optionDefault = document.createElement("option")
-            optionDefault.value = product.Category.id
-            optionDefault.innerHTML = product.Category.name
-            selectCategory.appendChild(optionDefault)
-            
-            categories.forEach((category)=>{
-                if(optionDefault.value != category.id){
-                    let categoryOption = document.createElement("option");
-                    categoryOption.value = category.id;
-                    categoryOption.innerHTML = category.name;
-                    selectCategory.appendChild(categoryOption); 
-                }
+            divNombre.appendChild(inputNombre);
+            div.appendChild(divNombre)
 
-                /* let categoryOption = document.createElement("option");
-                categoryOption.value = category.id;
-                categoryOption.innerHTML = category.name;
-                selectCategory.appendChild(categoryOption); */
-            })
-            div.appendChild(selectCategory);
-
+            let divBoton = document.createElement("div");
+            divBoton.className = 'claseFormulario';
             let boton = document.createElement("button");
+            boton.innerHTML = "Editar"
             boton.addEventListener("click", async() => {
-                await fetch('/api/productos/'+product.id, {
+                await fetch('/api/categorias/'+category.id, {
                     method: "PUT",
                     headers:{
                         "Content-Type": "application/json" // formencode
                     },
                     body: JSON.stringify({
-                        name: inputNombre.value,
-                        description: inputDescription.value,
-                        price: inputPrice.value,
-                        category_id: selectCategory.value
+                        name: inputNombre.value
                     })
                 })
                 cargarProductos();
             })
-            div.appendChild(boton);
-            ul.innerHTML = "";
-            ul.appendChild(div);
+            divBoton.appendChild(boton)
+            div.appendChild(divBoton);
+            cats.innerHTML = "";
+            cats.className = ""
+            /* ul.className = "" */
+            /* h1.innerHTML = "FORMULARIO DE EDICIÓN" */
+            cats.appendChild(div);
         })
-        li.appendChild(iEditar);
-        
-        // Icono borrar
+
+        let botonEliminar = document.createElement('button')
+        botonEliminar.setAttribute('class','btn btn-secondary')
+        botonEliminar.setAttribute('title','Eliminar categoría')
+        botonEliminar.setAttribute('type', 'button')
         let iBorrar = document.createElement('i');
         iBorrar.setAttribute("class", "fa-solid fa-trash");
+        botonEliminar.appendChild(iBorrar)
+
         iBorrar.addEventListener("click", async() => {
-            await fetch('/api/productos/'+product.id, {
+            await fetch('/api/categorias/'+category.id, {
                 method: "DELETE"
             })
             cargarProductos();
         })
-        li.appendChild(iBorrar);
-        
 
-        ul.appendChild(li);
-    })
-
-
-    return products;
-}
-
-async function cargarProductosCliente(){
-    let data = await fetch('/api/productos');
-    console.log(data)
-    let products = await data.json();
-    console.log(products)
-
-    data = await fetch('/api/categorias');
-    console.log(data)
-    let categories = await data.json();
-    console.log(categories)
-
-    let ul = document.querySelector("ul#productos");
-    ul.innerHTML = "";
-    
-    products.forEach(function(product){
-        let li = document.createElement('li');
+        overlay.appendChild(botonEditar)
+        overlay.appendChild(botonEliminar)
 
         let img = document.createElement("img");
-        img.src = "/img/products/"+product.image;
-        li.appendChild(img);
-
-        // ID
-        let spanId = document.createElement('span');
-        spanId.innerHTML = `${product.id} - `;
-        li.appendChild(spanId);
+        if(category.Products.length > 0){
+            img.src = "/img/products/"+category.Products[0].image;
+        }else{
+            img.src = "/img/products/pan.png"
+        }
         
-        // Nombre
-        let spanName = document.createElement('span');
-        spanName.innerHTML = product.name;
-        li.appendChild(spanName);
 
-        // Precio
-        let spanPrice = document.createElement('span');
-        spanPrice.innerHTML = ` $${product.price}`;
-        li.appendChild(spanPrice);
+        let cardBody = document.createElement('div')
+        cardBody.className = 'card-body'
+        let hNombre = document.createElement('h3')
+        hNombre.innerHTML = category.name
+        let hBoton = document.createElement('h6')
+        let buttonBoton = document.createElement('button')
+        let aBoton = document.createElement('a')
+        aBoton.setAttribute('class', 'pan')
+        aBoton.setAttribute('href', '/panaderia')
+        aBoton.innerHTML = 'Catálogo'
+        buttonBoton.appendChild(aBoton)
+        hBoton.appendChild(buttonBoton)
 
-        // Categoría
-        let spanCategory = document.createElement('span');
-        spanCategory.innerHTML = ` ${product.Category.name}`;
-        li.appendChild(spanCategory);
+        cardBody.appendChild(hNombre)
+        cardBody.appendChild(hBoton)
 
-        ul.appendChild(li);
+        card.appendChild(overlay)
+        card.appendChild(img)
+        card.appendChild(cardBody)
+        col.appendChild(card)
+
+        cats.appendChild(col)
+
     })
 
+    let divCrear = document.createElement('div')
+    divCrear.setAttribute('class', "col-md-3 py-3 py-md-0 boton")
+    //Crear producto
+    let createButton = document.createElement("i");
+    createButton.setAttribute("class", "fa-solid fa-plus");
+    createButton.addEventListener('click', () => {
+        let div = document.createElement("div");
+        div.setAttribute('id','form');
+
+        let divName = document.createElement("div");
+        divName.className = 'claseFormulario';
+        let labelNombre = document.createElement("label")
+        labelNombre.innerHTML = "Nombre: "
+        divName.appendChild(labelNombre)
+        let inputNombre = document.createElement("input"); // Name, type y id
+        //inputNombre.setAttribute('required', '');
+        divName.appendChild(inputNombre);
+        div.appendChild(divName)
+
+
+        let divBoton = document.createElement("div");
+        divBoton.className = 'claseFormulario';
+        let boton = document.createElement("button");
+        boton.innerHTML = "Añadir"
+        boton.addEventListener("click", async() => {
+            if(inputNombre.value != ""){
+                await fetch('/api/categorias', {
+                    method: "POST",
+                    headers:{
+                        "Content-Type": "application/json" // formencode
+                    },
+                    body: JSON.stringify({
+                        name: inputNombre.value
+                    })
+                })
+    
+                cargarProductos();
+            }
+        })
+        divBoton.appendChild(boton)
+        div.appendChild(divBoton);
+        cats.innerHTML = ""
+        cats.className = ""
+        cats.appendChild(div);
+    })
+    divCrear.appendChild(createButton);
+    cats.appendChild(divCrear)
 
     return products;
 }
